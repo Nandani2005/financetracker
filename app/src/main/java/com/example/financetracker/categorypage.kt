41714bab9b335777d4262d2f57d3fbd8ae5ef41c
Sriyanshi Sinha <sriyanshisinha1974@gmail.com>
2025-06-27 20:37:50 +0530

    Commit done

---
+++ app/src/main/java/com/example/financetracker/categorypage.kt
@@ -0,0 +1,48 @@
+package com.example.financetracker
+
+
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class categorypage : AppCompatActivity() {
+
+    private lateinit var mySpinner: Spinner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categorypage)
+
+        mySpinner = findViewById(R.id.mySpinner)
+
+        // Dropdown list items
+        val items = arrayOf("Select Item", "Apple", "Banana", "Cherry", "Date")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        mySpinner.adapter = adapter
+
+        mySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val selected = parent.getItemAtPosition(position).toString()
+                if (position != 0) { // skip "Select Item"
+                    Toast.makeText(this@categorypage, "Selected: $selected", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+        }
+    }
+}

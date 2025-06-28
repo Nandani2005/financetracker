ac81f01fcd0c2f097d01cbe1eeb311c155ae20ad
Sriyanshi Sinha <sriyanshisinha1974@gmail.com>
2025-06-28 09:59:50 +0530

    Commit done

--- app/src/main/java/com/example/financetracker/homepageactivity.kt
+++ app/src/main/java/com/example/financetracker/homepageactivity.kt
@@ -1,6 +1,8 @@
 package com.example.financetracker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,5 +18,15 @@ class homepageactivity : AppCompatActivity() {
         val nameWrite=findViewById<TextView>(R.id.nameView)
 
         nameWrite.append(name)
+        val btn =findViewById<Button>(R.id.category)
+        btn.setOnClickListener(){
+            val tr=Intent(this@homepageactivity,categorypage::class.java)
+            startActivity(tr)
+        }
+        val hisbtn =findViewById<Button>(R.id.history1)
+        hisbtn.setOnClickListener(){
+            val tr=Intent(this@homepageactivity,history::class.java)
+            startActivity(tr)
+        }
     }
 }
